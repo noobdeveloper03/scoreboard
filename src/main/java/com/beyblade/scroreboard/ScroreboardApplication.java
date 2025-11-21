78326f5681ed84b4c7899b1bdff39a46ea63cc18
noobdeveloper03 <noob.developer03@gmail.com>
2025-11-21 14:53:22 +0800

    Initialize

---
+++ src/main/java/com/beyblade/scroreboard/ScroreboardApplication.java
@@ -0,0 +1,13 @@
+package com.beyblade.scroreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScroreboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScroreboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beyblade/scroreboard/ScroreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyblade.scroreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScroreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
